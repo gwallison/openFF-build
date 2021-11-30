e3934736bda87e7b4b269ab6798e7c4bc999bb5a
allisong <gary.allison@gmail.com>
2021-11-30 17:01:51 -0500

    remove TSCA and WellExplorer external references; not used

--- core/external_dataset_tools.py
+++ core/external_dataset_tools.py
@@ -31,31 +31,33 @@ def add_Elsner_table(df,sources='./sources/',
                   how='left',validate='1:1')
     return mg
 
-def add_WellExplorer_table(df,sources='./sources/',
-                     outdir='./out/',
-                     wefn='well_explorer_corrected.csv'):
-    """Add the WellExplorer data table. """
-    #print('Adding WellExplorer table to CAS table')
-    wedf = pd.read_csv(sources+wefn)
-    #print(wedf.head())
-    # checking overlap first:
 # =============================================================================
-#     wecas = list(wedf.we_CASNumber.unique())
-#     dfcas = list(df.bgCAS.unique())
-#     with open(outdir+'wellexplorer_non_overlap.txt','w') as f:
-#         f.write('**** bgCAS numbers without an WellExplorer entry: *****\n')
-#         for c in dfcas:
-#             if c not in wecas:
-#                 f.write(f'{c}\n')
-#         f.write('\n\n***** WellExplorer CAS numbers without a FF entry: *****\n')
-#         for c in wecas:
-#             if c not in dfcas:
-#                 f.write(f'{c}\n')
-# 
+# def add_WellExplorer_table(df,sources='./sources/',
+#                      outdir='./out/',
+#                      wefn='well_explorer_corrected.csv'):
+#     """Add the WellExplorer data table. """
+#     #print('Adding WellExplorer table to CAS table')
+#     wedf = pd.read_csv(sources+wefn)
+#     #print(wedf.head())
+#     # checking overlap first:
+# # =============================================================================
+# #     wecas = list(wedf.we_CASNumber.unique())
+# #     dfcas = list(df.bgCAS.unique())
+# #     with open(outdir+'wellexplorer_non_overlap.txt','w') as f:
+# #         f.write('**** bgCAS numbers without an WellExplorer entry: *****\n')
+# #         for c in dfcas:
+# #             if c not in wecas:
+# #                 f.write(f'{c}\n')
+# #         f.write('\n\n***** WellExplorer CAS numbers without a FF entry: *****\n')
+# #         for c in wecas:
+# #             if c not in dfcas:
+# #                 f.write(f'{c}\n')
+# # 
+# # =============================================================================
+#     mg = pd.merge(df,wedf,left_on='bgCAS',right_on='we_CASNumber',
+#                   how='left',validate='1:1')
+#     return mg
 # =============================================================================
-    mg = pd.merge(df,wedf,left_on='bgCAS',right_on='we_CASNumber',
-                  how='left',validate='1:1')
-    return mg
 
     
 def add_TEDX_ref(df,sources='./sources/',
@@ -66,13 +68,15 @@ def add_TEDX_ref(df,sources='./sources/',
     df['is_on_TEDX'] = df.bgCAS.isin(tedx_cas)
     return df
     
-def add_TSCA_ref(df,sources='./sources/',
-                 tsca_fn = 'TSCAINV_092019.csv'):
-    #print('Adding TSCA to CAS table')
-    tscadf = pd.read_csv(sources+tsca_fn)
-    tsca_cas = tscadf.CASRN.unique().tolist()
-    df['is_on_TSCA'] = df.bgCAS.isin(tsca_cas)
-    return df
+# =============================================================================
+# def add_TSCA_ref(df,sources='./sources/',
+#                  tsca_fn = 'TSCAINV_092019.csv'):
+#     #print('Adding TSCA to CAS table')
+#     tscadf = pd.read_csv(sources+tsca_fn)
+#     tsca_cas = tscadf.CASRN.unique().tolist()
+#     df['is_on_TSCA'] = df.bgCAS.isin(tsca_cas)
+#     return df
+# =============================================================================
     
 def add_Prop65_ref(df,sources='./sources/',
                  p65_fn = 'p65list12182020.csv'):
@@ -134,9 +138,9 @@ def add_all_bgCAS_tables(df,sources='./sources/external_refs/',
     #df = add_CWA_primary_ref(df,sources) 
     df = add_CompTox_refs(df,sources)
     df = add_Prop65_ref(df,sources)
-    df = add_TSCA_ref(df,sources)
+    #df = add_TSCA_ref(df,sources)
     df = add_TEDX_ref(df,sources)
-    df = add_WellExplorer_table(df,sources,outdir)
+    #df = add_WellExplorer_table(df,sources,outdir)
     df = add_Elsner_table(df,sources,outdir)
     df = add_PFAS_ref(df,sources)
     df = add_Volatile_ref(df,sources)
